fbb1dbd205a741b10132b4d22fc75bad86d21d87
easylearningbd <elebdsoft@gmail.com>
2023-12-05 04:07:07 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">
+     Kazi Ariyan
+    </h1>
+    </div>
+  );
+}
+
+export default App;
