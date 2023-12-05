4c10683dcbb36e5e245c7db69bf8af398f1d2d42
easylearningbd <elebdsoft@gmail.com>
2023-12-06 04:17:58 +0600

    p7 Route And Login Register Part 3

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React,{lazy} from 'react';
 import ReactDOM from 'react-dom/client'; 
- 
+import './index.css';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 const App = lazy(() => import('./App'))
--- src/views/auth/Register.jsx
+++ src/views/auth/Register.jsx
@@ -2,8 +2,23 @@ import React from 'react';
 
 const Register = () => {
     return (
-        <div>
-            <h1>Register Page </h1>  
+        <div className='min-w-screen min-h-screen bg-[#cdcae9] flex justify-center items-center' >
+          <div className='w-[350px] text-[#ffffff] p-2'>
+            <div className='bg-[#6f68d1] p-4 rounded-md'>
+                <h2 className='text-xl mb-3 font-bold'>Welcome to Ecommerce</h2>
+                <p className='text-sm mb-3 font-medium'>Please register your account</p>
+
+    <form>
+        <div className='flex flex-col w-full gap-1 mb-3'>
+            <label htmlFor="name">Name</label>
+            <input className='px-3 py-2 outline-none border border-slate-400 bg-transparent rounded-md' type="text" name='name' placeholder='Name' id='name' required />
+
+        </div>
+    </form>
+ 
+            </div>
+            </div>  
+            
         </div>
     );
 };
