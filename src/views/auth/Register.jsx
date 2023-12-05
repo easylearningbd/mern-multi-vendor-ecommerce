8507ae9a4d922c240cbb087364f9495d37792015
easylearningbd <elebdsoft@gmail.com>
2023-12-06 05:15:01 +0600

    p9 Route And Login Register Part 5

--- src/views/auth/Register.jsx
+++ src/views/auth/Register.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
+import { FaGoogle } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
 
 const Register = () => {
     return (
@@ -36,9 +38,28 @@ const Register = () => {
         <button className='bg-slate-800 w-full hover:shadow-blue-300/ hover:shadow-lg text-white rounded-md px-7 py-2 mb-3'>Sign Up</button>
 
         <div className='flex items-center mb-3 gap-3 justify-center'>
-            <p>Already Have an account ? <Link className='font-bold' to="/login">Sing In</Link> </p>
+            <p>Already Have an account ? <Link className='font-bold' to="/login">Sing In</Link> </p> 
+        </div>
+
+        <div className='w-full flex justify-center items-center mb-3'>
+            <div className='w-[45%] bg-slate-700 h-[1px]'></div>
+            <div className='w-[10%] flex justify-center items-center'>
+                <span className='pb-1'>Or</span>
+            </div>
+            <div className='w-[45%] bg-slate-700 h-[1px] '></div>
+        </div>
+
+        <div className='flex justify-center items-center gap-3'>
+            <div className='w-[135px] h-[35px] flex rounded-md bg-orange-700 shadow-lg hover:shadow-orange-700/50 justify-center cursor-pointer items-center overflow-hidden'>
+            <span><FaGoogle /></span>
+             </div>
+
+             <div className='w-[135px] h-[35px] flex rounded-md bg-blue-700 shadow-lg hover:shadow-blue-700/50 justify-center cursor-pointer items-center overflow-hidden'>
+            <span><FaFacebook /></span>
+             </div>
 
         </div>
+
 
     </form>
  
