29db73231fe12e2bfd9417a9325153c0596df8ce
easylearningbd <elebdsoft@gmail.com>
2023-12-15 02:35:36 +0600

    p36 Project Header Design Part 3

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -10,6 +10,24 @@ const Header = ({showSidebar, setShowSidebar}) => {
           <span><FaList/></span>
         </div>
 
+        <div className='hidden md:block'>
+          <input className='px-3 py-2 outline-none border bg-transparent border-slate-700 rounded-md text-[#423d72] focus:border-indigo-300 overflow-hidden' type="text" name='search' placeholder='search' />
+        </div>
+
+        <div className='flex justify-center items-center gap-8 relative'>
+          <div className='flex justify-center items-center'>
+            <div className='flex justify-center items-center gap-3'>
+              <div className='flex justify-center items-center flex-col text-end'>
+          <h2 className='text-md font-bold'>Kazi Ariyan</h2>
+          <span className='text-[14px] w-full font-normal'>Admin</span>
+              </div>
+              <img className='w-[45px] h-[45px] rounded-full overflow-hidden' src="http://localhost:3000/images/admin.jpg" alt="" />
+
+            </div>
+          </div>
+
+        </div>
+
 
           </div> 
         </div>
