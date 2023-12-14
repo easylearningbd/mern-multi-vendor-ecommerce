e4612653c43bd208b2c3c0441817c9e7fa42f49e
easylearningbd <elebdsoft@gmail.com>
2023-12-15 01:19:52 +0600

    p34 Project Header Design Part 1

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { FaList } from 'react-icons/fa';
 
-const Header = () => {
+const Header = ({showSidebar, setShowSidebar}) => {
     return (
-        <div>
-          <h1>Header </h1>
+        <div className='fixed top-0 left-0 w-full py-5 px-2 lg:px-7 z-40'>
+          <div className='ml-0 lg:ml-[260px] rounded-md h-[65px] flex justify-between items-start bg-[#b1addf] px-5 transition-all'>
+
+        <div onClick={() => setShowSidebar(!showSidebar)} className='w-[35px] flex lg:hidden' >
+          <span><FaList/></span>
+        </div>
+
+
+          </div> 
         </div>
     );
 };
