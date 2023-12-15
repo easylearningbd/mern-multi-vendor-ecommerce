250021844c76cc578aabf233d5482f39010e96f9
easylearningbd <elebdsoft@gmail.com>
2023-12-16 01:08:38 +0600

    p41 Dashboard Design Part 5

--- src/views/admin/AdminDashboard.jsx
+++ src/views/admin/AdminDashboard.jsx
@@ -200,6 +200,47 @@ const AdminDashboard = () => {
         </div>
 
 
+        <div className='w-full p-4 bg-[#6a5fdf] rounded-md mt-6'>
+            <div className='flex justify-between items-center'>
+                <h2 className='font-semibold text-lg text-[#d0d2d6] pb-3 '>Recent Orders</h2>
+                <Link className='font-semibold text-sm text-[#d0d2d6]'>View All</Link>
+               </div>
+
+    <div className='relative overflow-x-auto'>
+    <table className='w-full text-sm text-left text-[#d0d2d6]'>
+        <thead className='text-sm text-[#d0d2d6] uppercase border-b border-slate-700'>
+        <tr>
+            <th scope='col' className='py-3 px-4'>Order Id</th>
+            <th scope='col' className='py-3 px-4'>Price</th>
+            <th scope='col' className='py-3 px-4'>Payment Status</th>
+            <th scope='col' className='py-3 px-4'>Order Status</th>
+            <th scope='col' className='py-3 px-4'>Active</th>
+        </tr>
+        </thead>
+
+        <tbody>
+            <tr>
+                <td scope='row' className='py-3 px-4 font-medium whitespace-nowrap'>#34344</td>
+                <td scope='row' className='py-3 px-4 font-medium whitespace-nowrap'>$454</td>
+                <td scope='row' className='py-3 px-4 font-medium whitespace-nowrap'>Pending</td>
+                <td scope='row' className='py-3 px-4 font-medium whitespace-nowrap'>Pending</td>
+                <td scope='row' className='py-3 px-4 font-medium whitespace-nowrap'>View</td>
+            </tr>
+
+            <tr>
+                <td scope='row' className='py-3 px-4 font-medium whitespace-nowrap'>#34344</td>
+                <td scope='row' className='py-3 px-4 font-medium whitespace-nowrap'>$454</td>
+                <td scope='row' className='py-3 px-4 font-medium whitespace-nowrap'>Pending</td>
+                <td scope='row' className='py-3 px-4 font-medium whitespace-nowrap'>Pending</td>
+                <td scope='row' className='py-3 px-4 font-medium whitespace-nowrap'>View</td>
+            </tr>
+        </tbody>
+
+    </table>
+
+    </div>
+
+        </div>
 
 
 
