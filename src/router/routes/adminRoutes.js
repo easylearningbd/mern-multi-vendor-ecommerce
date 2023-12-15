63b34bb1c8a8e56352227292d3bb6c93871ab2c8
easylearningbd <elebdsoft@gmail.com>
2023-12-16 01:55:57 +0600

    p43 Admin Order Page Design Part 1

--- src/router/routes/adminRoutes.js
+++ src/router/routes/adminRoutes.js
@@ -1,10 +1,16 @@
-import { lazy } from "react";   
-const AdminDashboard = lazy(()=> import('../../views/admin/AdminDashboard'))   
+import { lazy } from "react";    
+const AdminDashboard = lazy(()=> import('../../views/admin/AdminDashboard'))  
+const Orders = lazy(()=> import('../../views/admin/Orders'))    
 
 export const adminRoutes = [
     {
         path: 'admin/dashboard',
         element : <AdminDashboard/>,
+        role : 'admin'
+    },
+    {
+        path: 'admin/dashboard/orders',
+        element : <Orders/> ,
         role : 'admin'
     }
 
---
+++ src/views/admin/Orders.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+const Orders = () => {
+    const [currentPage, setCurrentPage] = useState(1)
+    const [searchValue, setSearchValue] = useState('')
+    const [parPage, setParPage] = useState(5)
+
+    return (
+        <div className='px-2 lg:px-7 pt-5'>
+            <div className='w-full p-4 bg-[#6a5fdf] rounded-md'>
+                <div className='flex justify-between items-center'>
+                    <select onChange={(e) => setParPage(parseInt(e.target.value))} className='px-4 py-2 focus:border-indigo-500 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]'>
+                        <option value="5">5</option>
+                        <option value="10">10</option>
+                        <option value="20">20</option> 
+                    </select>
+                    <input className='px-4 py-2 focus:border-indigo-500 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]' type="text" placeholder='search' />
+
+                </div>
+
+            </div>
+           
+        </div>
+    );
+};
+
+export default Orders;
