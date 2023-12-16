0fa41ae15068391120c0cb3820d9678e2239141d
easylearningbd <elebdsoft@gmail.com>
2023-12-17 04:04:47 +0600

    p54 Sellers Page Design

--- src/router/routes/adminRoutes.js
+++ src/router/routes/adminRoutes.js
@@ -2,6 +2,7 @@ import { lazy } from "react";
 const AdminDashboard = lazy(()=> import('../../views/admin/AdminDashboard'))  
 const Orders = lazy(()=> import('../../views/admin/Orders')) 
 const Category = lazy(()=> import('../../views/admin/Category'))  
+const Sellers = lazy(()=> import('../../views/admin/Sellers'))  
 
 export const adminRoutes = [
     {
@@ -18,6 +19,11 @@ export const adminRoutes = [
         path: 'admin/dashboard/category',
         element : <Category/> ,
         role : 'admin'
+    },
+    {
+        path: 'admin/dashboard/sellers',
+        element : <Sellers/> ,
+        role : 'admin'
     }
-
+ 
 ]
---
+++ src/views/admin/Sellers.jsx
@@ -0,0 +1,94 @@
+import React, { useState } from 'react'; 
+import { Link } from 'react-router-dom';
+import Pagination from '../Pagination';
+import { FaEye } from "react-icons/fa";
+
+const Sellers = () => {
+    const [currentPage, setCurrentPage] = useState(1)
+    const [searchValue, setSearchValue] = useState('')
+    const [parPage, setParPage] = useState(5)
+    const [show, setShow] =  useState(false)
+
+    return (
+        <div className='px-2 lg:px-7 pt-5'>
+             <div className='w-full p-4 bg-[#6a5fdf] rounded-md'>
+            
+             <div className='flex justify-between items-center'>
+                    <select onChange={(e) => setParPage(parseInt(e.target.value))} className='px-4 py-2 focus:border-indigo-500 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]'>
+                        <option value="5">5</option>
+                        <option value="10">10</option>
+                        <option value="20">20</option> 
+                    </select>
+                    <input className='px-4 py-2 focus:border-indigo-500 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]' type="text" placeholder='search' /> 
+                </div>
+
+                <div className='relative overflow-x-auto'>
+    <table className='w-full text-sm text-left text-[#d0d2d6]'>
+        <thead className='text-sm text-[#d0d2d6] uppercase border-b border-slate-700'>
+        <tr>
+            <th scope='col' className='py-3 px-4'>No</th>
+            <th scope='col' className='py-3 px-4'>Image</th>
+            <th scope='col' className='py-3 px-4'>Name</th>
+            <th scope='col' className='py-3 px-4'>Shop Name</th> 
+            <th scope='col' className='py-3 px-4'>Payment Status</th> 
+            <th scope='col' className='py-3 px-4'>Email</th> 
+            <th scope='col' className='py-3 px-4'>Devision</th> 
+            <th scope='col' className='py-3 px-4'>District</th> 
+            <th scope='col' className='py-3 px-4'>Action</th> 
+        </tr>
+        </thead>
+
+        <tbody>
+            {
+                [1,2,3,4,5].map((d, i) => <tr key={i}>
+                <td scope='row' className='py-1 px-4 font-medium whitespace-nowrap'>{d}</td>
+                <td scope='row' className='py-1 px-4 font-medium whitespace-nowrap'>
+                    <img className='w-[45px] h-[45px]' src={`http://localhost:3000/images/category/${d}.jpg`} alt="" />
+                </td>
+                <td scope='row' className='py-1 px-4 font-medium whitespace-nowrap'>Kazi Ariyan </td>
+                <td scope='row' className='py-1 px-4 font-medium whitespace-nowrap'>Easy Shop</td>
+                <td scope='row' className='py-1 px-4 font-medium whitespace-nowrap'>
+                    <span>Pending</span> </td>
+                <td scope='row' className='py-1 px-4 font-medium whitespace-nowrap'>ariyan@gmail.com </td>
+
+                <td scope='row' className='py-1 px-4 font-medium whitespace-nowrap'>Dhaka </td>
+
+                <td scope='row' className='py-1 px-4 font-medium whitespace-nowrap'>Paltan </td>
+                 
+                <td scope='row' className='py-1 px-4 font-medium whitespace-nowrap'>
+                    <div className='flex justify-start items-center gap-4'>
+                    <Link className='p-[6px] bg-green-500 rounded hover:shadow-lg hover:shadow-green-500/50'> <FaEye /> </Link> 
+                    
+                    </div>
+                    
+                    </td>
+            </tr> )
+            }
+
+            
+        </tbody> 
+    </table> 
+    </div>  
+
+    <div className='w-full flex justify-end mt-4 bottom-4 right-4'>
+        <Pagination 
+            pageNumber = {currentPage}
+            setPageNumber = {setCurrentPage}
+            totalItem = {50}
+            parPage = {parPage}
+            showItem = {3}
+        />
+        </div>
+
+
+
+
+
+
+             </div>
+            
+        </div>
+    );
+};
+
+export default Sellers;
