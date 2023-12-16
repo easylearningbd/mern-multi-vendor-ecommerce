f261bd7427b44257b2edf68be377cd775148e15f
easylearningbd <elebdsoft@gmail.com>
2023-12-16 19:18:49 +0600

    p47 Create Pagination Part 2

--- src/views/Pagination.jsx
+++ src/views/Pagination.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { MdOutlineKeyboardDoubleArrowLeft } from "react-icons/md";
+
 
 const Pagination = ({pageNumber,setPageNumber,totalItem,parPage,showItem}) => {
 
@@ -15,12 +17,29 @@ const Pagination = ({pageNumber,setPageNumber,totalItem,parPage,showItem}) => {
         startPage = 1
     }
 
-    const createBton = () => {
+    const createBtn = () => {
 
+        const btns = []
+        for (let i = startPage; i < endPage; i++) {
+            btns.push(
+                <li onClick={()=>setPageNumber(i)} className={` ${pageNumber === i ? 'bg-indigo-300 shadow-lg shadow-indigo-300/50 text-white' : 'bg-slate-600 hover:bg-indigo-400 shadow-lg hover:shadow-indigo-500/50 hover:text-white text-[#d0d2d6]'} w-[33px] h-[33px] rounded-full flex justify-center items-center cursor-pointer `}>
+                    {i}                    
+                </li>
+            ) 
+        }
+        return btns
     }
+
     return (
-        <ul>
-            
+        <ul className='flex gap-3'>
+            {
+                pageNumber > 1 && <li className='w-[33px] h-[33px] rounded-full flex justify-center items-center bg-slate-300 text-[#000000]'>
+                    <MdOutlineKeyboardDoubleArrowLeft />
+                </li>
+            }{
+                createBtn()
+            }
+
         </ul>
     )
 
