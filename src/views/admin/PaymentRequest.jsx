a44febe904765f2bcc39d7db701ca2aeac632924
easylearningbd <elebdsoft@gmail.com>
2023-12-18 02:32:50 +0600

    p56 Payment Request Page Design Part 2

--- src/views/admin/PaymentRequest.jsx
+++ src/views/admin/PaymentRequest.jsx
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import { FixedSizeList as List } from 'react-window';
 
+function handleOnWheel({ deltaY }) {
+    console.log('handleOnWheel',deltaY)
+}
+
+const outerElementType = forwardRef((props, ref) => (
+    <div ref={ref} onWheel={handleOnWheel} {...props} /> 
+ ))
+
 const PaymentRequest = () => {
+
+    const array = [1,2,3,4,5,6,7,8,9,10]
+
+    const Row = ({ index, style }) => {
+        return (
+        <div style={style} className='flex text-sm text-white font-medium'>
+        <div className='w-[25%] p-2 whitespace-nowrap'>{index + 1}</div>
+        <div className='w-[25%] p-2 whitespace-nowrap'>$3434</div>
+        <div className='w-[25%] p-2 whitespace-nowrap'>
+            <span className='py-[1px] px-[5px] bg-slate-300 text-blue-500 rounded-md text-sm'>Pending</span>
+         </div>
+        <div className='w-[25%] p-2 whitespace-nowrap'> 25 Dec 2023 </div>
+        <div className='w-[25%] p-2 whitespace-nowrap'>
+            <button className='bg-indigo-500 shadow-lg hover:shadow-indigo-500/50 px-3 py-[2px cursor-pointer text-white rounded-sm text-sm]'>Confirm</button>
+        </div>
+
+            </div>
+        )
+    }
+
+
+
+
+
     return (
 <div className='px-2 lg:px-7 pt-5'>
     <div className='w-full p-4 bg-[#6a5fdf] rounded-md'>
@@ -14,8 +46,20 @@ const PaymentRequest = () => {
                     <div className='w-[25%] p-2'> Status </div>
                     <div className='w-[25%] p-2'> Date </div>
                     <div className='w-[25%] p-2'> Action </div> 
-
                 </div>
+                {
+                    <List
+                    style={{ minWidth : '340px'}}
+                    className='List'
+                    height={350}
+                    itemCount={10}
+                    itemSize={35}
+                    outerElementType={outerElementType}                    
+                    >
+                        {Row}
+
+                    </List>
+                }
 
             </div>
 
