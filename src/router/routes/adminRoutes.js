fef1697acfb7eca7995ebcc98198b6b8d51678e3
easylearningbd <elebdsoft@gmail.com>
2023-12-22 02:29:17 +0600

    p59 Deactive Sellers Request Page Design Part 2

--- src/router/routes/adminRoutes.js
+++ src/router/routes/adminRoutes.js
@@ -1,10 +1,11 @@
-import { lazy } from "react";     
+import { lazy } from "react";      
 const AdminDashboard = lazy(()=> import('../../views/admin/AdminDashboard'))  
 const Orders = lazy(()=> import('../../views/admin/Orders')) 
 const Category = lazy(()=> import('../../views/admin/Category'))  
 const Sellers = lazy(()=> import('../../views/admin/Sellers'))
 const PaymentRequest = lazy(()=> import('../../views/admin/PaymentRequest'))  
-const DeactiveSellers = lazy(()=> import('../../views/admin/DeactiveSellers'))    
+const DeactiveSellers = lazy(()=> import('../../views/admin/DeactiveSellers'))  
+const SellerRequest = lazy(()=> import('../../views/admin/SellerRequest'))     
 
 export const adminRoutes = [
     {
@@ -35,6 +36,11 @@ export const adminRoutes = [
     {
         path: 'admin/dashboard/deactive-sellers',
         element : <DeactiveSellers/> ,
+        role : 'admin'
+    },
+    {
+        path: 'admin/dashboard/sellers-request',
+        element : <SellerRequest/> ,
         role : 'admin'
     }
  
