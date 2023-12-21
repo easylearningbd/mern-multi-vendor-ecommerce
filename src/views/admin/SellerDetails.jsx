700c404ce6a5435d0349e56975816ff32ba6f4d5
easylearningbd <elebdsoft@gmail.com>
2023-12-22 03:21:43 +0600

    p61 Deactive Sellers Request Page Design Part 4

--- src/views/admin/SellerDetails.jsx
+++ src/views/admin/SellerDetails.jsx
@@ -42,22 +42,65 @@ const SellerDetails = () => {
             <span>Active </span> 
         </div>
 
-    </div>
-
-                </div>
-
+    </div> 
+         </div> 
             </div>
 
 
+            <div className='w-4/12'>
+                <div className='px-0 md:px-5 py-2'>
+                    <div className='py-2 text-lg'>
+                        <h2>Address</h2>
+                    </div>
+
+    <div className='flex justify-between text-sm flex-col gap-2 p-4 bg-[#9e97e9] rounded-md'>
+        <div className='flex gap-2 font-bold text-[#000000]'>
+            <span>Shop Name : </span>
+            <span>Easy Shop </span> 
+        </div>
+        <div className='flex gap-2 font-bold text-[#000000]'>
+            <span>Divission : </span>
+            <span>Dhaka </span> 
+        </div>
+
+        <div className='flex gap-2 font-bold text-[#000000]'>
+            <span>District : </span>
+            <span>Dhaka  </span> 
+        </div>
+        <div className='flex gap-2 font-bold text-[#000000]'>
+            <span>State : </span>
+            <span>Palton </span> 
+        </div>
+        
+
+    </div> 
+         </div> 
+            </div>
+ 
+        </div> 
 
 
+        <div>
+            <form>
+                <div className='flex gap-4 py-3'>
+                    <select className='px-4 py-2 focus:border-indigo-500 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]' name="" id="">
+                        <option value="">--Select Status--</option>
+                        <option value="active">Active</option>
+                        <option value="deactive">Deactive</option>
+                    </select>
+                    <button className='bg-red-500 w-[170px] hover:shadow-red-500/40 hover:shadow-md text-white rounded-md px-7 py-2'>Submit</button>
+
+                </div>
+            </form>
         </div>
 
 
 
 
-        </div>
 
+
+
+        </div> 
         </div>
     );
 };
