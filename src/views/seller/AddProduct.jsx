fa4facbd01b916b8e29260921c9a59d72e3247bd
easylearningbd <elebdsoft@gmail.com>
2023-12-24 06:02:37 +0600

    p78 Product Image Add Manage Part 3

--- src/views/seller/AddProduct.jsx
+++ src/views/seller/AddProduct.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { IoMdImages } from "react-icons/io";
+import { IoMdCloseCircle } from "react-icons/io";
 
 const AddProduct = () => {
     const categorys = [
@@ -96,6 +97,14 @@ const AddProduct = () => {
         }
     }
 
+    const removeImage = (i) => {
+        const filterImage = images.filter((img,index) => index !== i)
+        const filterImageUrl = imageShow.filter((img, index) => index !== i )
+
+        setImages(filterImage)
+        setImageShow(filterImageUrl)
+    }
+
     return (
         <div className='px-2 lg:px-7 pt-5'>
             <div className='w-full p-4 bg-[#6a5fdf] rounded-md'>
@@ -177,6 +186,7 @@ const AddProduct = () => {
                         <img className='w-full h-full rounded-sm' src={img.url} alt="" />
                     </label>
                     <input onChange={(e)=> changeImage(e.target.files[0],i) } type="file" id={i} className='hidden'/>
+                    <span onClick={()=>removeImage(i)} className='p-2 z-10 cursor-pointer bg-slate-700 hover:shadow-lg hover:shadow-slate-400/50 text-white absolute top-1 right-1 rounded-full'><IoMdCloseCircle /></span>
                 </div> )
                }
                
@@ -185,6 +195,11 @@ const AddProduct = () => {
                     <span>Select Image </span>
                 </label>
                 <input className='hidden' onChange={imageHandle} multiple type="file" id='image' />
+
+            </div>
+
+            <div className='flex'>
+                <button className='bg-red-500  hover:shadow-red-500/40 hover:shadow-md text-white rounded-md px-7 py-2 my-2'>Add Product</button>
 
             </div>
 
