cac5231a1f0363a474f3e6e2fbfff8b6c566d8cd
easylearningbd <elebdsoft@gmail.com>
2023-12-25 03:32:03 +0600

    p85 Orders And Payments  Page Design Part 4

--- src/views/seller/Payments.jsx
+++ src/views/seller/Payments.jsx
@@ -1,9 +1,33 @@
-import React from 'react';
-import { MdCurrencyExchange,MdProductionQuantityLimits } from "react-icons/md";
-import { FaUsers } from "react-icons/fa";
-import { FaCartShopping } from "react-icons/fa6"; 
+import React, { forwardRef } from 'react';
+import { MdCurrencyExchange,MdProductionQuantityLimits } from "react-icons/md"; 
+import { FixedSizeList as List } from 'react-window';
+
+function handleOnWheel({ deltaY }) {
+    console.log('handleOnWheel',deltaY)
+}
+
+const outerElementType = forwardRef((props, ref) => (
+    <div ref={ref} onWheel={handleOnWheel} {...props} /> 
+ ))
 
 const Payments = () => {
+ 
+    const Row = ({ index, style }) => {
+        return (
+        <div style={style} className='flex text-sm text-white font-medium'>
+        <div className='w-[25%] p-2 whitespace-nowrap'>{index + 1}</div>
+        <div className='w-[25%] p-2 whitespace-nowrap'>$3434</div>
+        <div className='w-[25%] p-2 whitespace-nowrap'>
+            <span className='py-[1px] px-[5px] bg-slate-300 text-blue-500 rounded-md text-sm'>Pending</span>
+         </div>
+        <div className='w-[25%] p-2 whitespace-nowrap'> 25 Dec 2023 </div>
+        
+
+            </div>
+        )
+    }
+
+
     return (
         <div className='px-2 md:px-7 py-5'>
            <div className='w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-7 mb-5'>
@@ -63,7 +87,7 @@ const Payments = () => {
                 <div className='pt-5 mb-5'>
                     <form>
                         <div className='flex gap-3 flex-wrap'>
-                            <input min='0' type="number" className='px-3 py-2 md:w-[79%] focus:border-indigo-200 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]' name='amount' />
+                            <input min='0' type="number" className='px-3 py-2 md:w-[75%] focus:border-indigo-200 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]' name='amount' />
                             <button className='bg-red-500  hover:shadow-red-500/40 hover:shadow-md text-white rounded-md px-7 py-2'>Submit</button>
  
                         </div>
@@ -72,17 +96,71 @@ const Payments = () => {
 
                 <div>
                     <h2 className='text-lg pb-4'>Pending Request </h2>
-                </div>
 
+                    <div className='w-full overflow-x-auto'>
+                <div className='flex bg-[#a7a3de] uppercase text-xs font-bold min-w-[340px] rounded-md'>
+                    <div className='w-[25%] p-2'> No </div>
+                    <div className='w-[25%] p-2'> Amount </div>
+                    <div className='w-[25%] p-2'> Status </div>
+                    <div className='w-[25%] p-2'> Date </div> 
+                </div>
+                {
+                    <List
+                    style={{ minWidth : '340px'}}
+                    className='List'
+                    height={350}
+                    itemCount={10}
+                    itemSize={35}
+                    outerElementType={outerElementType}                    
+                    >
+                        {Row}
+
+                    </List>
+                }
+
+            </div> 
+                </div> 
             </div>
 
-        </div>    
+
+
+
+            <div className='bg-[#6a5fdf] text-[#d0d2d6] rounded-md p-5'>
+                
+                <div>
+                    <h2 className='text-lg pb-4'>Success WithDrawal </h2>
+
+                    <div className='w-full overflow-x-auto'>
+                <div className='flex bg-[#a7a3de] uppercase text-xs font-bold min-w-[340px] rounded-md'>
+                    <div className='w-[25%] p-2'> No </div>
+                    <div className='w-[25%] p-2'> Amount </div>
+                    <div className='w-[25%] p-2'> Status </div>
+                    <div className='w-[25%] p-2'> Date </div> 
+                </div>
+                {
+                    <List
+                    style={{ minWidth : '340px'}}
+                    className='List'
+                    height={350}
+                    itemCount={10}
+                    itemSize={35}
+                    outerElementType={outerElementType}                    
+                    >
+                        {Row}
+
+                    </List>
+                }
+
+            </div> 
+                </div> 
+            </div>
 
 
 
 
+            
 
-
+        </div>   
 
         </div>
     );
