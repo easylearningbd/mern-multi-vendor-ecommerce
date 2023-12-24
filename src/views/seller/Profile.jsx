bbb9063148a2e42ebcde6f679d63db2633ecadc8
easylearningbd <elebdsoft@gmail.com>
2023-12-25 05:49:09 +0600

    p90 Seller Profile Page Design Part 2

--- src/views/seller/Profile.jsx
+++ src/views/seller/Profile.jsx
@@ -3,7 +3,7 @@ import { FaImages } from "react-icons/fa6";
 import { FadeLoader } from 'react-spinners';
 
 const Profile = () => {
-    const image = false 
+    const image = true 
     const loader = true
 
     return (
@@ -13,7 +13,19 @@ const Profile = () => {
                     <div className='w-full p-4 bg-[#6a5fdf] rounded-md text-[#d0d2d6]'>
                         <div className='flex justify-center items-center py-3'>
                             {
-                                image ? <label htmlFor=""></label> : <label className='flex justify-center items-center flex-col h-[150px] w-[200px] cursor-pointer border border-dashed hover:border-red-500 border-[#d0d2d6] relative' htmlFor="img">
+                                image ? <label htmlFor="img" className='h-[150px] w-[200px] relative p-3 cursor-pointer overflow-hidden'>
+                                    <img src="http://localhost:3000/images/demo.jpg" alt="" />
+                                    {
+                                    !loader && <div className='bg-slate-600 absolute left-0 top-0 w-full h-full opacity-70 flex justify-center items-center z-20'>
+                                        <span>
+                                            <FadeLoader/>
+                                        </span>
+
+                                    </div>
+                                }
+
+
+                                </label> : <label className='flex justify-center items-center flex-col h-[150px] w-[200px] cursor-pointer border border-dashed hover:border-red-500 border-[#d0d2d6] relative' htmlFor="img">
                                 <span><FaImages /> </span>
                                 <span>Select Image</span>
                                 {
@@ -27,6 +39,7 @@ const Profile = () => {
 
                             </label>
                             }
+                            <input type="file" className='hidden' id='img' />
 
                         </div>
 
