deaa5061446cd18fb89b0cc75e33998e353d7395
easylearningbd <elebdsoft@gmail.com>
2023-12-26 02:18:29 +0600

    p93 Seller Profile Page Design Part 5

--- src/views/seller/Profile.jsx
+++ src/views/seller/Profile.jsx
@@ -78,7 +78,7 @@ const Profile = () => {
 
         <div className='px-0 md:px-5 py-2'>
             {
-                userInfo && <form>
+                !userInfo ? <form>
                     <div className='flex flex-col w-full gap-1 mb-2'>
                 <label htmlFor="Shop">Shop Name</label>
                 <input className='px-4 py-2 focus:border-indigo-200 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]' type="text" name='shopName' id='Shop' placeholder='Shop Name' />
@@ -101,7 +101,26 @@ const Profile = () => {
 
             <button className='bg-red-500  hover:shadow-red-500/40 hover:shadow-md text-white rounded-md px-7 py-2 my-2'>Save Changes</button>
 
-                </form>
+                </form> : <div className='flex justify-between text-sm flex-col gap-2 p-4 bg-slate-800 rounded-md relative'>
+                <span className='p-[6px] bg-yellow-500 rounded hover:shadow-lg hover:shadow-yellow-500/50 absolute right-2 top-2 cursor-pointer'><FaRegEdit /> </span>
+                <div className='flex gap-2'>
+                    <span>Shop Name : </span>
+                    <span>Easy Shop</span> 
+                </div>
+                <div className='flex gap-2'>
+                    <span>Divission : </span>
+                    <span>Dhaka</span> 
+                </div>
+                <div className='flex gap-2'>
+                    <span>District : </span>
+                    <span>Rajbari</span> 
+                </div>
+                <div className='flex gap-2'>
+                    <span>Sub District : </span>
+                    <span>Vola</span> 
+                </div>
+                
+            </div> 
             }
 
 
@@ -118,8 +137,38 @@ const Profile = () => {
 
 
     <div className='w-full md:w-6/12'>
+        <div className='w-full pl-0 md:pl-7 mt-6 md:mt-0'>
+        <div className='bg-[#6a5fdf] rounded-md text-[#d0d2d6] p-4'>
+        <h1 className='text-[#d0d2d6] text-lg mb-3 font-semibold'>Change Password</h1>
+        <form>
+                    <div className='flex flex-col w-full gap-1 mb-2'>
+                <label htmlFor="email">Email</label>
+                <input className='px-4 py-2 focus:border-indigo-200 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]' type="email" name='email' id='email' placeholder='email' />
+            </div>  
+
+            <div className='flex flex-col w-full gap-1 mb-2'>
+                <label htmlFor="o_password">Old Password</label>
+                <input className='px-4 py-2 focus:border-indigo-200 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]' type="password" name='old_password' id='o_password' placeholder='Old Password' />
+            </div>  
+
+            <div className='flex flex-col w-full gap-1 mb-2'>
+                <label htmlFor="n_password">New Password</label>
+                <input className='px-4 py-2 focus:border-indigo-200 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]' type="password" name='new_password' id='n_password' placeholder='New Password' />
+            </div>   
+ 
+
+            <button className='bg-red-500  hover:shadow-red-500/40 hover:shadow-md text-white rounded-md px-7 py-2 my-2'>Save Changes</button>
+
+                </form>
 
         </div>
+        
+        </div>
+
+    </div>
+
+
+
 
 </div>
             
