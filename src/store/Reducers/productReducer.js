d2e14c9f02d447b9c891f832cc0d20fcdf75f021
easylearningbd <elebdsoft@gmail.com>
2024-01-07 03:51:01 +0600

    p133 Product Edit and Update Part 3

--- src/store/Reducers/productReducer.js
+++ src/store/Reducers/productReducer.js
@@ -55,6 +55,26 @@ export const get_product = createAsyncThunk(
 )
 
   // End Method 
+
+
+  
+export const update_product = createAsyncThunk(
+    'product/update_product',
+    async( product ,{rejectWithValue, fulfillWithValue}) => {
+        
+        try {
+             
+            const {data} = await api.get(`/product-update`, product,{withCredentials: true}) 
+            console.log(data)
+            return fulfillWithValue(data)
+        } catch (error) {
+            // console.log(error.response.data)
+            return rejectWithValue(error.response.data)
+        }
+    }
+)
+
+  // End Method 
  
 export const productReducer = createSlice({
     name: 'product',
--- src/views/seller/EditProduct.jsx
+++ src/views/seller/EditProduct.jsx
@@ -4,7 +4,10 @@ import { IoMdImages } from "react-icons/io";
 import { IoMdCloseCircle } from "react-icons/io";
 import { useDispatch, useSelector } from 'react-redux';
 import { get_category } from '../../store/Reducers/categoryReducer';
-import { get_product } from '../../store/Reducers/productReducer';
+import { get_product,update_product } from '../../store/Reducers/productReducer';
+import { PropagateLoader } from 'react-spinners';
+import { overrideStyle } from '../../utils/utils';
+import toast from 'react-hot-toast';
 
 const EditProduct = () => {
 
@@ -12,7 +15,7 @@ const EditProduct = () => {
 
     const dispatch = useDispatch()
     const { categorys } = useSelector(state => state.category)
-    const { product } = useSelector(state => state.product)
+    const { product,loader,successMessage,errorMessage } = useSelector(state => state.product)
     
     useEffect(() => {
         dispatch(get_category({
@@ -90,6 +93,50 @@ const EditProduct = () => {
         setCategory(product.category)
         setImageShow( product.images)
     },[product])
+
+
+
+    useEffect(() => {
+
+        if (successMessage) {
+            toast.success(successMessage)
+            dispatch(messageClear()) 
+            setState({
+                name: "",
+                description: '',
+                discount: '',
+                price: "",
+                brand: "",
+                stock: ""
+            })  
+            setCategory('')
+
+        }
+        if (errorMessage) {
+            toast.error(errorMessage)
+            dispatch(messageClear())
+        }
+        
+
+    },[successMessage,errorMessage])
+
+
+    const update = (e) => {
+        e.preventDefault()
+        const obj = {
+                name: state.name,
+                description: state.description,
+                discount: state.discount,
+                price: state.price,
+                brand: state.brand,
+                stock: state.stock,
+                productId: productId
+        }
+        dispatch(update_product(obj))
+
+    }
+ 
+
  
     return (
         <div className='px-2 lg:px-7 pt-5'>
@@ -99,7 +146,7 @@ const EditProduct = () => {
                     <Link to='/seller/dashboard/products' className='bg-blue-500 hover:shadow-blue-500/50 hover:shadow-lg text-white rounded-sm px-7 py-2 my-2'>All Product</Link> 
                 </div>
 <div>
-    <form>
+    <form onSubmit={update}>
         <div className='flex flex-col mb-3 md:flex-row gap-4 w-full text-[#d0d2d6]'>
             <div className='flex flex-col w-full gap-1'>
                 <label htmlFor="name">Product Name</label>
@@ -178,7 +225,11 @@ const EditProduct = () => {
             </div>
 
             <div className='flex'>
-                <button className='bg-red-500  hover:shadow-red-500/40 hover:shadow-md text-white rounded-md px-7 py-2 my-2'>Save Changes</button>
+            <button disabled={loader ? true : false}  className='bg-red-500 w-[280px] hover:shadow-red-300/50 hover:shadow-lg text-white rounded-md px-7 py-2 mb-3'>
+            {
+               loader ? <PropagateLoader color='#fff' cssOverride={overrideStyle} /> : 'Save Changes'
+            } 
+            </button>
 
             </div>
 
