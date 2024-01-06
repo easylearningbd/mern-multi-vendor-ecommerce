b8ce79ad782c74b89f9d7a9901816bc7c8eed34c
easylearningbd <elebdsoft@gmail.com>
2024-01-06 06:13:02 +0600

    p130 Product Show Part 3 Bug Fixed

--- src/store/Reducers/productReducer.js
+++ src/store/Reducers/productReducer.js
@@ -63,8 +63,7 @@ export const productReducer = createSlice({
         }) 
         .addCase(add_product.fulfilled, (state, { payload }) => {
             state.loader = false;
-            state.successMessage = payload.message
-            state.products = [...state.products, payload.category]
+            state.successMessage = payload.message 
              
         })
 
