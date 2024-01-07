5a98a79031e60459cc30ffc5687e6d9c8212ce20
easylearningbd <elebdsoft@gmail.com>
2024-01-08 05:31:17 +0600

    p145 Seller Request Show Add Part 3

--- src/store/Reducers/sellerReducer.js
+++ src/store/Reducers/sellerReducer.js
@@ -10,7 +10,7 @@ export const get_seller_request = createAsyncThunk(
         try {
              
             const {data} = await api.get(`/request-seller-get?page=${page}&&searchValue=${searchValue}&&parPage=${parPage}`,{withCredentials: true}) 
-            // console.log(data)
+             console.log(data)
             return fulfillWithValue(data)
         } catch (error) {
             // console.log(error.response.data)
@@ -39,13 +39,13 @@ export const sellerReducer = createSlice({
 
     },
     extraReducers: (builder) => {
-        // builder
+        builder
           
-        // .addCase(get_category.fulfilled, (state, { payload }) => {
-        //     state.totalCategory = payload.totalCategory;
-        //     state.categorys = payload.categorys;
+        .addCase(get_seller_request.fulfilled, (state, { payload }) => {
+            state.sellers = payload.sellers;
+            state.totalSeller = payload.totalSeller;
              
-        // })
+        })
  
 
     }
--- src/views/admin/SellerRequest.jsx
+++ src/views/admin/SellerRequest.jsx
@@ -49,20 +49,20 @@ const SellerRequest = () => {
 
         <tbody>
             {
-                [1,2,3,4,5].map((d, i) => <tr className='border-b border-slate-700' key={i}>
-                <td scope='row' className='py-2 px-4 font-medium whitespace-nowrap'>{d}</td> 
-                <td scope='row' className='py-2 px-4 font-medium whitespace-nowrap'>Kazi Ariyan </td>
-                <td scope='row' className='py-2 px-4 font-medium whitespace-nowrap'>ariyan@gmail.com</td>
+                sellers.map((d, i) => <tr className='border-b border-slate-700' key={i}>
+                <td scope='row' className='py-2 px-4 font-medium whitespace-nowrap'>{i+1}</td> 
+                <td scope='row' className='py-2 px-4 font-medium whitespace-nowrap'>{d.name} </td>
+                <td scope='row' className='py-2 px-4 font-medium whitespace-nowrap'>{d.email} </td>
                 <td scope='row' className='py-2 px-4 font-medium whitespace-nowrap'>
-                    <span>Inactive</span> </td>
+                    <span>{d.payment}</span> </td>
 
                <td scope='row' className='py-2 px-4 font-medium whitespace-nowrap'>
-                    <span>Pending</span> </td>
+                    <span>{d.status}</span> </td>
                  
                  
                 <td scope='row' className='py-2 px-4 font-medium whitespace-nowrap'>
                     <div className='flex justify-start items-center gap-4'>
-                    <Link to='/admin/dashboard/seller/details/2' className='p-[6px] bg-green-500 rounded hover:shadow-lg hover:shadow-green-500/50'> <FaEye /> </Link> 
+                    <Link to={`/admin/dashboard/seller/details/${d._id}`} className='p-[6px] bg-green-500 rounded hover:shadow-lg hover:shadow-green-500/50'> <FaEye /> </Link> 
                     
                     </div>
                     
