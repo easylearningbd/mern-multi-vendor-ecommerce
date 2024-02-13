e02b7b42643c78d3bdbfa6eeda7496f9862d30e6
easylearningbd <elebdsoft@gmail.com>
2024-02-14 01:55:01 +0600

    p314 Seller to Customer Chat Complet Part 2

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,9 +1,22 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from './Header'; 
 import Sidebar from './Sidebar';
+import { socket } from '../utils/utils'
+import { useSelector } from 'react-redux';
 
 const MainLayout = () => {
+
+
+    const {userInfo } = useSelector(state => state.auth)
+
+    useEffect(() => {
+        if (userInfo && userInfo.role === 'seller') {
+            socket.emit('add_seller', userInfo._id,userInfo)
+        } else {
+            socket.emit('add_admin', userInfo)
+        }
+    },[userInfo])
 
     const [showSidebar, setShowSidebar] = useState(false)
 
