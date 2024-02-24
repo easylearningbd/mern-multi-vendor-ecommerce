33f809a41504cac4b53a61682ff6cb9882c0c709
easylearningbd <elebdsoft@gmail.com>
2024-02-24 21:03:50 +0600

    p357 Seller Order Details Get and Show Part 4

--- src/store/Reducers/OrderReducer.js
+++ src/store/Reducers/OrderReducer.js
@@ -70,6 +70,19 @@ export const get_admin_orders = createAsyncThunk(
 ) 
   // End Method  
 
+  export const seller_order_status_update = createAsyncThunk(
+    'orders/seller_order_status_update',
+    async( {orderId,info} ,{rejectWithValue, fulfillWithValue}) => { 
+        try { 
+            const {data} = await api.put(`/seller/order-status/update/${orderId}`,info,{withCredentials: true})  
+            return fulfillWithValue(data)
+        } catch (error) { 
+            return rejectWithValue(error.response.data)
+        }
+    }
+) 
+  // End Method  
+
  
 export const OrderReducer = createSlice({
     name: 'order',
@@ -112,7 +125,13 @@ export const OrderReducer = createSlice({
         .addCase(get_seller_order.fulfilled, (state, { payload }) => {
             state.order = payload.order; 
         })
-        
+
+        .addCase(seller_order_status_update.rejected, (state, { payload }) => {
+            state.errorMessage = payload.message; 
+        })
+        .addCase(seller_order_status_update.fulfilled, (state, { payload }) => {
+            state.successMessage = payload.message; 
+        })
  
 
     }
--- src/views/seller/OrderDetails.jsx
+++ src/views/seller/OrderDetails.jsx
@@ -1,30 +1,53 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
-import { get_seller_order } from '../../store/Reducers/OrderReducer';
+import { get_seller_order,messageClear, seller_order_status_update } from '../../store/Reducers/OrderReducer';
+import toast from 'react-hot-toast';
 
 const OrderDetails = () => {
 
     const { orderId } = useParams() 
     const dispatch = useDispatch() 
+    const [status, setStatus] = useState('')
+
     const { order,errorMessage,successMessage } = useSelector(state => state.order)
+
+    useEffect(() => {
+        setStatus(order?.delivery_status)
+    },[order])
+
 
     useEffect(() => {
         dispatch(get_seller_order(orderId))
     },[orderId])
 
+    const status_update = (e) => {
+        dispatch(seller_order_status_update({orderId, info: {status: e.target.value} }))
+        setStatus(e.target.value)
+    }
+
+    useEffect(() => { 
+        if (successMessage) {
+            toast.success(successMessage)
+            dispatch(messageClear())  
+        } 
+        if (errorMessage) {
+            toast.error(errorMessage)
+            dispatch(messageClear())  
+        } 
+    },[successMessage,errorMessage])
 
     return (
         <div className='px-2 lg:px-7 pt-5'>
         <div className='w-full p-4 bg-[#6a5fdf] rounded-md'>
             <div className='flex justify-between items-center p-4'>
                 <h2 className='text-xl text-[#d0d2d6]'>Order Details</h2>
-                <select name="" id="" className='px-4 py-2 focus:border-indigo-500 outline-none bg-[#475569] border border-slate-700 rounded-md text-[#d0d2d6]'>
-                <option value="">pending</option>
-                <option value="">processing</option>
-                <option value="">warehouse</option>
-                <option value="">placed</option>
-                <option value="">cancelled</option>
+                <select onChange={status_update} value={status} name="" id="" className='px-4 py-2 focus:border-indigo-500 outline-none bg-[#475569] border border-slate-700 rounded-md text-[#d0d2d6]'>
+                <option value="pending">pending</option>
+                <option value="processing">processing</option>
+                <option value="warehouse">warehouse</option>
+                <option value="placed">placed</option>
+                <option value="cancelled">cancelled</option>
                 </select> 
             </div>
 
